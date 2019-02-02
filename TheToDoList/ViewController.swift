6fd3b1c1038de5abc84cf21c06b200b4d0c57590
work <work@222.local>
2019-02-02 18:00:50 +0100

    Initial Commit

---
+++ TheToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheToDoList
+//
+//  Created by work on 2/2/19.
+//  Copyright © 2019 Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
