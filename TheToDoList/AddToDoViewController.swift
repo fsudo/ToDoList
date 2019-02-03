e416c6709c64e8f6175064e11f370f854ddb8f46
studio <studi0@protonmail.com>
2019-02-03 04:52:00 +0100

    Final Code Version for the To Do list

--- TheToDoList/AddToDoViewController.swift
+++ TheToDoList/AddToDoViewController.swift
@@ -23,7 +23,7 @@ class AddToDoViewController: UIViewController {
 
     @IBAction func addTapped(_ sender: Any) {
         
-      /*
+ /*
         let toDo = ToDo()
         
         if let titleText = titleTextField.text {
@@ -37,6 +37,20 @@ class AddToDoViewController: UIViewController {
         }
  */
     
+        if let context = (UIApplication.shared.delegate as?
+            AppDelegate)?.persistentContainer.viewContext {
+    
+            let toDo = ToDoCoreData(entity: ToDoCoreData.entity(), insertInto: context)
+        
+            if let titleText = titleTextField.text {
+                toDo.name = titleText
+                toDo.important = importantSwitch.isOn
+            }
+            
+            try? context.save()
+            
+            navigationController?.popViewController(animated: true)
+        }
     }
     
 }
--- TheToDoList/ToDoTableViewController.swift
+++ TheToDoList/ToDoTableViewController.swift
@@ -9,22 +9,38 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
-
-    var toDos : [ToDo] = []
+    
+    var toDos : [ToDoCoreData] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    toDos = createToDos()
-       
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+         getToDos()
+    }
+    
+    func getToDos( ) {
+        if let context = (UIApplication.shared.delegate as?
+            AppDelegate)?.persistentContainer.viewContext {
+            
+            if let coreDataToDos = try? context.fetch( ToDoCoreData.fetchRequest()) as?
+                [ToDoCoreData] {
+                if let theToDos = coreDataToDos {
+                    toDos = theToDos
+                    tableView.reloadData()
+                }
+            }
+        }
+    }
+    
     func createToDos() -> [ToDo] {
         
         let eggs = ToDo()
         eggs.name = "Buy Eggs"
         eggs.important = true
-       
+        
         let dog = ToDo()
         dog.name = "Walk The Dog"
         
@@ -34,29 +50,31 @@ class ToDoTableViewController: UITableViewController {
         return [eggs, dog, cheese]
     }
     
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
         -> Int {
-        return toDos.count
+            return toDos.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         let toDo = toDos[indexPath.row]
         
-        if toDo.important {
-            cell.textLabel?.text = "❗️" + toDo.name
-        }   else {
-             cell.textLabel?.text = toDo.name
+        if let name = toDo.name {
+            
+            if toDo.important {
+                cell.textLabel?.text = "❗️" + name
+            }   else {
+                cell.textLabel?.text = toDo.name
+            }
         }
-
         return cell
     }
-   
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-        {
+    {
         let toDo = toDos[indexPath.row]
         performSegue(withIdentifier: "moveToComplete", sender: toDo)
     }
@@ -67,8 +85,8 @@ class ToDoTableViewController: UITableViewController {
         }
         
         if let completeVC = segue.destination as? CompleteToDoViewController {
-           
-            if let toDo = sender as? ToDo {
+            
+            if let toDo = sender as? ToDoCoreData {
                 completeVC.selectedToDo = toDo
                 completeVC.previousVC = self
             }
